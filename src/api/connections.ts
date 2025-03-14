e7c3077d5fb37f8587b85203d422f9df97ff9bb7
SergBily <sergbily.dev@gmail.com>
2025-03-14 11:45:51 +0300

    refactor: connection api

--- src/api/connections.ts
+++ src/api/connections.ts
@@ -2,14 +2,14 @@ import axios from "axios";
 import { API_BASE_URL } from "../config";
 
 const BASE_URL: string = API_BASE_URL;
-const API_STATUSDATA = "api/connector/status/";
+const API_STATUS_DATA = "api/connector/status/";
 const API_CONNECTIONS = "api/connector/connections/";
-const GET_CONNECTIONS = "api/order/get-connections/";
-const SETTINGS_DATA_SOURCE = "settings/data-source";
+const API_GET_CONNECTIONS = "api/order/get-connections/";
+const API_SETTINGS_DATA_SOURCE = "settings/data-source";
 
-const axiosConfig = (cookies: string) => ({
+const createAxiosConfig = (token: string) => ({
   headers: {
-    Authorization: cookies,
+    Authorization: token,
     "ngrok-skip-browser-warning": "true",
   },
 });
@@ -17,33 +17,33 @@ const axiosConfig = (cookies: string) => ({
 const constructUrl = (endpoint: string) => `${BASE_URL}${endpoint}`;
 
 export const getStatusData = (hostname: string, cookies: string) => {
-  return axios.get(constructUrl(API_STATUSDATA), axiosConfig(cookies));
+  return axios.get(constructUrl(API_STATUS_DATA), createAxiosConfig(cookies));
 };
 
 export const patchStatusData = (id: number, cookies: string, body: any) => {
   return axios.put(
     constructUrl(`${API_CONNECTIONS}${id}/`),
     body,
-    axiosConfig(cookies)
+    createAxiosConfig(cookies)
   );
 };
 
 export const getConnectionsToDatabases = (
   cookies: string
 ) => {
-  return axios.get(constructUrl(GET_CONNECTIONS), axiosConfig(cookies));
+  return axios.get(constructUrl(API_GET_CONNECTIONS), createAxiosConfig(cookies));
 };
 
-export const changeDataSource = async (cookies: string, source: string) => {
+export const updateDataSource = async (token: string, source: string) => {
   try {
     const response = await axios.post(
-      constructUrl(SETTINGS_DATA_SOURCE),
+      constructUrl(API_SETTINGS_DATA_SOURCE),
       { source },
-      axiosConfig(cookies)
+      createAxiosConfig(token)
     );
     return response.data;
   } catch (error) {
-    console.error("Error changing data source:", error);
+    console.error("Error updating data source:", error);
     throw error;
   }
 };
